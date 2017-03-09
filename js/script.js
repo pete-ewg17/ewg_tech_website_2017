b2f2b14195bf31fc274ab9c12eb43abcdf698daa
pete-ewg17 <peteakhurst@Petes-MacBook-Pro.local>
2017-03-09 14:56:37 +1100

    First Upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/*===========
+  BACK TO TOP 
+=============*/
+
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+    
+});
+
+/*=================
+  ANIMATE ON SCROLL
+=================*/
+
+AOS.init({
+  duration: 1200,
+});
+
+AOS.init({
+      disable: 'mobile'
+    });
+
+/*===========
+  FORM 
+=============*/
+
+$(function () {
+
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
+ 
