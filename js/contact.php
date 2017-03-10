90dcdfcc2059c88ba1c100634bf9093c50b7874e
pete-ewg17 <peteakhurst@Petes-MacBook-Pro.local>
2017-03-10 16:24:16 +1100

    Fixed logos and mobile responsiveness

---
+++ js/contact.php
@@ -0,0 +1,5 @@
+{
+  "result": true,
+  "type" : "warning",
+  "message" : "Thank you, we will be in touch soon."
+}
--- js/script.js
+++ js/script.js
@@ -1,61 +1,60 @@
 /*===========
   BACK TO TOP 
 =============*/
-
-$(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
-    
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn();
+        }
+        else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    $('#back-to-top').tooltip('show');
 });
-
 /*=================
   ANIMATE ON SCROLL
 =================*/
-
 AOS.init({
-  duration: 1200,
+    duration: 1200
+, });
+AOS.init({
+    disable: 'mobile'
 });
-
-AOS.init({
-      disable: 'mobile'
-    });
-
 /*===========
   FORM 
 =============*/
-
 $(function () {
-
     $('#contact-form').validator();
-
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
             var url = "contact.php";
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
+            var name = $("#contact_name").val();
+            var phone = $("#contact_phone").val();
+            var message = $("#contact_message").val();
+            var email = $("#contact_email").val();
+            $("#loading_div").css('display', 'block');
+            $.post('contact.php', {
+                n: name
+                , p: phone
+                , m: message
+                , e: email
+            }, function (result) {
+                console.log(result);
+                var j = $.parseJSON(result);
+                if (j.result) {
+                    $('#loading_div').css('display','none');
+                    $('#submitButton').css('display','none');
+                    var messageAlert = 'alert-' + j.type;
+                    var messageText = j.message;
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
@@ -67,14 +66,12 @@ $(function () {
         }
     })
 }); /* @Chris -- DOES IT GO AFTER THIS validation? */
-
 /*===========
   Counter 
 =============*/
-    $(document).ready(function(){
-        $('.counter-num').counterUp({
-            delay: 10,
-            time: 1000
-    });   
+$(document).ready(function () {
+    $('.counter-num').counterUp({
+        delay: 10
+        , time: 1000
+    });
 });
- 
