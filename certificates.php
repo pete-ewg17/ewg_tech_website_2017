45c6586761d88fd6f8b8d397db098ddfd14f429a
kariah-white-rosterfy <kariah.white@rosterfy.com.au>
2017-04-20 11:22:16 +1000

    Create certificates.php
    customize
    add elements
    preview
    upgrade

---
+++ certificates.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Cutting edge software to manage your event staff online. Including registration, online training and communication with event staff.">
+    <meta name="author" content="Event Workforce Group">
+    <meta property="og:site_name" content="Event Workforce Group | Technology" />
+    <meta property="og:title" content="Event Workforce Group Technology" />
+    <meta property="og:description" content="Cutting edge software to manage your event staff online. Including registration, online training and communication with event staff." />
+    <meta property="og:image" content="http://event-workforce.com.au/ewg_web/wordpress/wp-content/uploads/2016/10/cropped-EWG-Group-Logo.png">
+    <meta property="og:url" content="http://eventworkforcegroup.com/technology">
+    <title>EWG Technology | Rosterfy</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="css/hover.css" rel="stylesheet">
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!--FAVICONS-->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/ewg_favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/ewg_favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/ewg_favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/ewg_favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/ewg_favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/ewg_favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/ewg_favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/ewg_favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/ewg_favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/ewg_favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/ewg_favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/ewg_favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/ewg_favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/ewg_favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/ewg_favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+  <script>
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-96562946-1', 'auto');
+ ga('send', 'pageview');
+
+</script>
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php  if(isset($_GET['admin'])) { ?>
+    <style>
+    .content-section-a.white-top {
+    	padding-top:5em;
+    }	
+    </style>
+    <?php } ?>
+</head>
+
+<body>
+<?php  if(!isset($_GET['admin'])) { ?>
+   <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav animated fadeInDown" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar top-bar"></span> <span class="icon-bar middle-bar"></span> <span class="icon-bar bottom-bar"></span> </button>
+                <a class="navbar-brand topnav" href="index.html"><img class="logo" src="assets/ewg_tech_Logo.svg" alt="EWG Tech Logo"></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hvr-underline-from-center"> <a href="index.html">Home</a> </li>
+                    
+                    
+                       <!--DROP DOWN NAV-->
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">What We Do<span class="caret"></span></a>
+
+                           <ul class="dropdown-menu">
+                            <li><a href="rosterfy.html">Rosterfy</a></li>
+                            <li><a href="accreditation.html">Accreditation</a></li>
+                            <li><a href="training.html">Training</a></li>
+                            </ul>
+                        </li>    
+                    <li><a class="" href="demo.html"><span class="text-info">Request Demo</span></a></li>
+                    </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav><!-- /End Nav -->
+  
+<?php } ?>
+  <!-- EDIT BELOW THIS LINE -->
+  
+  
+   
+   <!--HEADER EDIT ME-->
+  <div class="content-section-a white-top">
+    <div class="container">
+      <div class="row featurette">
+        <div class="col-md-12 text-center" data-aos="fade-left" data-aos-once="true">
+          <h2>Certificates</h2>
+          <p>Certificates can be created to represent your organisation. They are a reward for you users who have assisted at an event.</p>      
+        </div>
+      </div>
+    </div>
+  </div>
+      
+      <!--EDIT ME-->
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div data-aos="fade-right" data-aos-once="true" class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">Customize and Design</h2>
+            <p class="tech-lead">Customize the height and width of your certificate and upload a background for the certificate. </p>
+          </div>
+            <div data-aos="fade-right" data-aos-once="true" class="col-md-5 col-md-pull-7">
+            <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Registration Form" style="background-size:cover; background-repeat:none;">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <!--EDIT ME-->    
+      <div class="content-section-a white">
+        <div class="container">
+          <div class="row featurette">
+            <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
+              <h2>Add Elements</h2>
+              <p>Add, edit and arrange images and text to sit on your certificate. </p>
+            </div>
+            <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
+              <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Scheduling and Group Management">
+            </div>
+          </div>
+      </div>
+    </div>
+      
+      <!--EDIT ME-->
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
+            <h2 class="featurette-heading">Preview</h2>
+            <p class="tech-lead">Preview the current layout or download the PDF version. </p>
+          </div>
+            <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
+            <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Enhanced Communication with Phone">
+            </div>
+          </div>
+        </div>
+      </div>
+     
+     <!--EDIT ME--> 
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
+            <h2 class="featurette-heading">Upgrade Your Account</h2> 
+            <p class="tech-lead">Upgrade to the Certificate feature to celebrate your users efforts.</p>
+          </div>
+            <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
+            <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Timesheets">
+            </div>
+          </div>
+        </div>
+  </div>
+      <!-- /END THE FEATURETTES -->
+      
+       <?php  if(!isset($_GET['admin'])) { ?>                                    
+<!-- DONT TOUCH ANYTHING BELOW THIS -->                                                                                                                     
+
+    <!--SOCIAL ICONS-->
+    <div class="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <ul class="list-inline banner-social-buttons">
+                        <li class="social-button"> <a href="http://www.facebook.com/EventWorkforceGroup/" class="btn btn-default btn-lg hvr-radial-in"><i class="fa fa-facebook fa-fw"></i></a> </li>
+                        <li class="social-button"> <a href="http://twitter.com/_ewgroup" class="btn btn-default btn-lg hvr-radial-in"><i class="fa fa-twitter fa-fw"></i></a> </li>
+                        <li class="social-button"> <a href="http://www.instagram.com/eventworkforcegroup/" class="btn btn-default btn-lg hvr-radial-in"><i class="fa fa-instagram fa-fw"></i></a> </li>
+                        <li class="social-button"> <a href="http://www.linkedin.com/company/eventworkforcegroup" class="btn btn-default btn-lg hvr-radial-in"><i class="fa fa-linkedin fa-fw"></i></a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.banner -->
+    
+    
+    <!-- Footer -->
+    <footer class="footer-img img-reponsive">
+        <div class="container">
+            <div class="row" align="center">
+                <div class="col-md-12">
+                    <ul class="list-inline">
+                        <li><a href="http://eventworkforcegroup.com/academy" target="_blank"><img src="assets/images/logos/ewg_acad_logo.svg" class="img-responsive footer-logo" alt="Academy Logo"> Academy</a> </li>
+                        <li><a href="http://eventworkforcegroup.com/talent" target="_blank"> <img src="assets/images/logos/ewg_talent_logo.svg" class="img-responsive footer-logo" alt="Academy Logo"> Talent</a> </li>
+                        <li><a href="http://eventworkforcegroup.com/technology" target="_blank"> <img src="assets/images/logos/ewg_tech_logo.svg" class="img-responsive footer-logo" alt="Academy Logo">Technology</a> </li>
+                    </ul>
+                    <p class="copyright text-muted small">Copyright &copy; Event Workforce Group 2017. All Rights Reserved</p> 
+                     <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <?php } ?>
+        
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <!--ANIMATE ON SCROLL -->
+        <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+        <!--VALIDATOR FOR CONTACT FORM-->
+        <script src="js/validator.js"></script>
+        <!--Web Script-->
+        <script src="js/script.js"></script>
+</body>
+
+</html>
