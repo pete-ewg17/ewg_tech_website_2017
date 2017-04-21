8d15affc2b1eb1e35136f1199cd8fa49c551fd2e
Bennett43 <bennett.merriman@rosterfy.com.au>
2017-04-21 14:20:21 +1000

    Updated text again
    Updated sales text

--- groupmanager.php
+++ groupmanager.php
@@ -109,7 +109,7 @@
       <div class="row featurette">
         <div class="col-md-12 text-center" data-aos="fade-left" data-aos-once="true">
           <h2>Group Manager</h2>
-          <p>Groups Manager enables you to create and manage your user groups. Whether it be a charity group, corporate group or operational team, group manager gives you the ability to best manager your workforce in groups and not just individually.</p>      
+          <p>Groups Manager enables you to create and manage your Workforce groups easily and effectively. Whether it be a charity group, corporate group or operational team, group manager gives you the ability to best manage your workforce in groups and not just individually.</p>      
         </div>
       </div>
     </div>
@@ -121,7 +121,7 @@
         <div class="row">
           <div data-aos="fade-right" data-aos-once="true" class="col-md-7 col-md-push-5">
             <h2 class="featurette-heading">Create User Groups</h2>
-            <p class="tech-lead">Create groups based on community groups, charities, event skill set and more. You can close events or shifts so that only pre-determined groups apply or confirm those assignments.</p>
+            <p class="tech-lead">Create groups based on community groups, charities, event skill set and more. You can close events or shifts so that only pre-determined groups apply or confirm the positions.</p>
           </div>
             <div data-aos="fade-right" data-aos-once="true" class="col-md-5 col-md-pull-7">
              <img class="featurette-image img-responsive img-rounded" src="assets/images/rosterfy/EWG-Tech_Rosterfy_Scheduling&GroupManagement_600X500.png" alt="Scheduling and Group Management">
@@ -151,7 +151,7 @@
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <h2 class="featurette-heading">Authenticated Member Lists</h2>
-            <p class="tech-lead">Dive deeper on the member lists to display further details about each individual. This will giev you greater transparency on these members with in the one page. Eg. See their work experience without needing to click into their profile.</p>
+            <p class="tech-lead">Dive deeper on the member lists to display further details about each individual. This will give you greater transparency on these members within the one page. Eg. See their work experience without needing to click into their profile.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Enhanced Communication with Phone">
@@ -181,7 +181,7 @@
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <h2 class="featurette-heading">Group Archive</h2> 
-            <p class="tech-lead">Archive dorment workforce groups. When using Rosterfy and archived groups, your lists to choose groups will be a lot shorter, and you can keep your old groups as a backup just in case they need to be called upon at a later date.</p>
+            <p class="tech-lead">Archive dorment workforce groups. When using Rosterfy and archived groups, your lists to choose groups will be a lot shorter and you can keep your old groups as a backup just in case they need to be called upon later.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Timesheets">
@@ -195,7 +195,7 @@
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true" data-aos-offset="400">
               <h2>Upload Workforce Groups to Shifts</h2>
-              <p class="lead"> To fill a shift quickly, you can easily import a Workforce Group. Eg. Perfect when needing to fill 20 positions at a drink station in one allocation.</p>
+              <p class="lead"> To fill a shift quickly, you can easily import a Workforce Group. Perfect for when you need to fill 20 positions at a drink station in one allocation.</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true" data-aos-offset="400">
               <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Workforce Portals Platform">
