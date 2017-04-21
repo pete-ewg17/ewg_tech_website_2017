2026e0509159d953e8651e145e546960624de886
Bennett43 <bennett.merriman@rosterfy.com.au>
2017-04-21 12:06:27 +1000

    Updated feature explanations.
    Adding some more content to each feature list, to sell a little more sales'y.

--- eventseries.php
+++ eventseries.php
@@ -109,7 +109,7 @@
       <div class="row featurette">
         <div class="col-md-12 text-center" data-aos="fade-left" data-aos-once="true">
           <h2>Event Series</h2>
-          <p>Create unique event series or brands or another defining category.</p>      
+          <p>Event series is a powerful tool allowing you to segment your event types to remain on brand with their look and feel and communications to your workforce.</p>      
         </div>
       </div>
     </div>
@@ -121,7 +121,7 @@
         <div class="row">
           <div data-aos="fade-right" data-aos-once="true" class="col-md-7 col-md-push-5">
             <h2 class="featurette-heading">Custom Look and Feel</h2>
-            <p class="tech-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos maxime voluptate repellendus sequi tempora cum, maiores mollitia, inventore, earum atque provident distinctio. Ex aspernatur quia velit dicta vitae molestiae quod.</p>
+            <p class="tech-lead">You control the branding images, colour schemes and email templates. This feature lets you publicly advertise event pages consistent with the branding requirements.</p>
           </div>
             <div data-aos="fade-right" data-aos-once="true" class="col-md-5 col-md-pull-7">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Registration Form" style="background-size:cover; background-repeat:none;">
@@ -136,7 +136,7 @@
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
               <h2>Email Templates</h2>
-              <p> Create specific emails to be sent for multiple event series.</p>
+              <p> Create specific emails to be sent for multiple event series. Update the the content, header and footer graphics to reflect the branding of the event series.</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
               <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Scheduling and Group Management">
@@ -150,8 +150,8 @@
        <div class="container">
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
-            <h2 class="featurette-heading">Training</h2>
-            <p class="tech-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat laboriosam saepe doloribus nisi delectus quibusdam nulla iste repellendus aut fuga laudantium minima voluptate labore ipsam eveniet, enim ipsum pariatur, sint!</p>
+            <h2 class="featurette-heading">Training Modules</h2>
+            <p class="tech-lead"> Training Modules often need to be specific to an event type. This custom feature provides you with the ability to generate training material specific to any event series within your account.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Enhanced Communication with Phone">
@@ -166,7 +166,7 @@
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true" data-aos-offset="400">
               <h2>Registration Settings </h2>
-              <p class="lead"> Setup a uniqie registration page and gather data from users registering based on a specific brand or event series.</p>
+              <p class="lead"> Setup a uniqie registration page and gather data from users registering based on a specific brand or event series. Recruit for multiple events types at the one time while keeping each event series seperate when your workforce register.</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true" data-aos-offset="400">
               <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Workforce Portals Platform">
@@ -181,7 +181,7 @@
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <h2 class="featurette-heading">Certificate Setup</h2> 
-            <p class="tech-lead"> Customize your post event certificates here to be branded or event series specific.</p>
+            <p class="tech-lead"> Customize your post event certificates to be branded or event series specific. We know how much your workforce will love branded certificates post event! Helping to encourage them to come back for your next event.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Timesheets">
