d94da829aaa1e47d63b59c83b5bb7f44f59ceda1
Bennett43 <bennett.merriman@rosterfy.com.au>
2017-04-28 15:29:49 +1000

    Updated Text to be more sales'y
    Update wording for each feature item.

--- sms.php
+++ sms.php
@@ -109,7 +109,7 @@
       <div class="row featurette">
         <div class="col-md-12 text-center" data-aos="fade-left" data-aos-once="true">
           <h2>SMS</h2>
-          <p>Track, manage and schedule SMS.</p>      
+          <p>Sending SMS is the most effective way to communicate with your workforce. You can send and track shift confirmations, important reminders and safety alerts during your event for the ultimate peace of mind. </p>      
         </div>
       </div>
     </div>
@@ -121,7 +121,7 @@
         <div class="row">
           <div data-aos="fade-right" data-aos-once="true" class="col-md-7 col-md-push-5">
             <h2 class="featurette-heading">SMS History</h2>
-            <p class="tech-lead">View your SMS History including delivery status, time, date and more. </p>
+            <p class="tech-lead">View your sent SMS history including delivery status, time, date and more. Keep track of who and when your workforce recieve SMS to have confidence your workforce are well informed. </p>
           </div>
             <div data-aos="fade-right" data-aos-once="true" class="col-md-5 col-md-pull-7">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Registration Form" style="background-size:cover; background-repeat:none;">
@@ -136,7 +136,7 @@
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
               <h2>SMS Templates</h2>
-              <p>Add new or edit existing SMS templates.</p>
+              <p>Forget the days of re-typing the same message, now you can create a template, include merge tags and send the SMS to one or several hundred individuals at the click of a button.</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
               <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Scheduling and Group Management">
@@ -151,7 +151,7 @@
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <h2 class="featurette-heading">Incoming SMS</h2>
-            <p class="tech-lead">Track SMS responses from your users.</p>
+            <p class="tech-lead">Track incoming SMS from your workforce within your portal gives you full visibility of incoming SMS so you can respond to your eager workforce.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Enhanced Communication with Phone">
@@ -166,7 +166,7 @@
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <h2 class="featurette-heading">Schedule SMS</h2> 
-            <p class="tech-lead">Get your communications ready in advance by scheduling your SMS.</p>
+            <p class="tech-lead">Going to be onsite, or need to send an SMS while you are out of the office? Why not schedule an SMS to send when it is most convenient for your workforce to read and respond on their terms.</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="500">
             <img class="featurette-image img-responsive img-rounded" src="http://placehold.it/600/500" alt="Timesheets">
