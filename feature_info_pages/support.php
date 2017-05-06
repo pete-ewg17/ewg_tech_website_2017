09a285d97894095cfd09f713c086cac4b8dc877d
bennett-merriman-rosterfy <BennettMerriman@Bennetts-MacBook-Pro.local>
2017-05-06 12:39:45 +1000

    Sales Wording
    updated support wording for sales team.

--- feature_info_pages/support.php
+++ feature_info_pages/support.php
@@ -12,7 +12,7 @@ include $file_base."template_files/header.php";
       <div class="row featurette">
         <div class="col-md-12 text-center" data-aos="fade-left" data-aos-once="true">
           <h2>Support</h2>
-          <p>Event Workforce Group Technology is here to support you. Use our various types of online support and knowledge base wherever you go or simply pick up the phone to speak to one of our experts. </p>      
+          <p>Event Workforce Group Technology is more than just a technology solution. We want your event to be a success no matter what challenges you face. Make use of our various online support features and industry leading knowledge base at anytime. We are the workforce management experts and by your side every step of the way. </p>      
         </div>
       </div>
     </div>
@@ -24,7 +24,7 @@ include $file_base."template_files/header.php";
         <div class="row">
           <div data-aos="fade-right" data-aos-once="true" class="col-md-7 col-md-push-5">
             <h2 class="featurette-heading">Online Support</h2>
-            <p class="tech-lead">Utlize our support documententation and submit a ticket if you require further assistance.</p>
+            <p class="tech-lead">Utlize our support documententation and FAQ's. Your team can also submit specific tickets if you require further assistance from our dedicated team of workforce experts. With multiple access permissions, our online support allows your team to view all support queries and our solutions in one portal. </p>
           </div>
             <div data-aos="fade-right" data-aos-once="true" class="col-md-5 col-md-pull-7">
             <img class="featurette-image no-shadow img-responsive img-rounded" src="../assets/feature_info_images/Zendesk.png" alt="Registration Form" style="background-size:cover; background-repeat:none;">
@@ -39,7 +39,7 @@ include $file_base."template_files/header.php";
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
               <h2>Knowledge Base</h2>
-              <p>Knowledge Base is home to some event industry insights, latest news, how to tutorials, frequently asked questions and plenty of other helpful resources</p>
+              <p>The EWG Knowledge Base is home to our unique industry insights, latest news, 'how to' tutorials, frequently asked questions and loads of resources at your fingertips. All within one place to guide and educate you every step of the way.</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
               <img class="featurette-image no-shadow img-responsive img-rounded" src="../assets/feature_info_images/Knowledge_Base.png" alt="Scheduling and Group Management">
@@ -54,7 +54,7 @@ include $file_base."template_files/header.php";
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <h2 class="featurette-heading">Virtual Tours</h2>
-            <p class="tech-lead"> Be guided through our step by step virtual tour for a specific page. </p>
+            <p class="tech-lead"> Account set up should be easy and it is! Be guided through our step by step virtual tour when you first launch your account or unlock features. No matter your level of computer literacy, we give you the ultimate peace of mind! </p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true" data-aos-offset="300">
             <img class="featurette-image no-shadow img-responsive img-rounded" src="../assets/feature_info_images/Virtual_Tour.png" alt="Enhanced Communication with Phone">
@@ -69,7 +69,7 @@ include $file_base."template_files/header.php";
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
               <h2>24/7 Support</h2>
-              <p>Enterprise only.</p>
+              <p> For enterprise clients, we offer 24/7 support, including phone support. We know events can throw up curve balls, rest assured we have seen them all before and will be there to guide and assist you. Your success is our success!</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
               <img class="featurette-image no-shadow img-responsive img-rounded" src="../assets/feature_info_images/Support24.png" alt="Scheduling and Group Management">
