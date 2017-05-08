ffaf0e01852be141440eb06eef79d3fb35f2d6a4
pete-ewg17 <peteakhurst-ewg@Petes-MacBook-Pro.local>
2017-05-08 12:47:40 +1000

    added pricing page

---
+++ pricing.php
@@ -0,0 +1,209 @@
+<?php
+$file_base = "";
+$title = "Pricing";
+$desc = "All your workforce needs";
+include $file_base."template_files/header.php";
+?>
+
+<div class="content-section-a white-top">
+<div class="container">
+  <div class="row featurette">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <h2 class="header">Pricing</h2>
+    <p>No matter how big, small or complex your workforce needs are, we have the solution for you.</p>
+    </div> 
+  </div>
+</div>
+</div>
+<br>
+
+<!-- Plans -->
+<section id="plans">
+  <div class="container">
+    <div class="row">
+      
+      <!-- item -->
+      <div class="col-md-3 text-center">
+       <div class="box">
+        <div class="panel panel-pricing">
+          <div class="panel-heading">
+            <i class="fa fa-users icon"></i>
+            <h3>Starter</h3>
+          </div>
+          <div class="panel-body text-center">
+            <p class="tag-text"><strong>Free</strong></p>
+           
+          </div>
+          <div class="panel-footer">
+            <a class="btn btn-lg btn-block btn-primary pricing_button" data-plan="free" href="#">Get Started</a>
+          </div>
+          <div class="panel-body-a text-center">
+            <p class="lead text-muted small" style="padding-top: 12px; color: #555; font-size: 12px;">For organisations wanting to build a workforce database but not yet scheduling for events.</p>
+                                    <br>
+                                            
+          </div>
+          <ul class="list-group text-center">
+            <li class="list-group-item"><i class="fa fa-check"></i> 1 Admin</li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/support.php" target="_blank"> <i class="fa fa-check"></i>Online Support </a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/customregistration.php" target="_blank"> <i class="fa fa-check"></i>Custom Registration Form </a></li>   
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/emails.php" target="_blank"><i class="fa fa-check"></i> Email Communications</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/accountbranding.php" target="_blank"> <i class="fa fa-check"></i>Custom Account Branding</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/userportal.php" target="_blank"><i class="fa fa-check"></i> Workforce Portals</a></li>
+            <li class="list-group-item-1"></li>
+           
+           
+          </ul>
+          <div class="border-bottom"></div>
+        </div>
+        </div>
+      </div>
+      <!-- /item -->
+
+      <!-- item -->
+      <div class="col-md-3 text-center">
+        <div class="box">
+        <div class="panel panel-pricing box">
+          <div class="panel-heading">
+            <i class="fa fa-cloud icon"></i>
+            <h3>Base</h3>
+          </div>
+          
+          <div class="panel-body text-center">
+            <p class="lead">Starting from<p>
+            <p><strong>$99 per month</strong></p>
+          </div>
+          <div class="panel-footer">
+            <a class="btn btn-lg btn-block btn-primary pricing_button" data-plan="base" href="#" target="_blank">Get Started</a>
+          </div>
+          <div class="panel-body-a text-center">
+            <p class="lead text-muted small" style="padding-top: 11px; color: #555; font-size: 12px;">Grass Roots Organisations with a small volunteer workforce (0-300) ready to begin scheduling.</p><br />
+                                    
+          </div>
+          <ul class="list-group text-center">
+            <li class="list-group-item first-item"><i class="fa fa-check"></i> Starter + </li>
+<br>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/event.php" target="_blank"><i class="fa fa-check"></i> Roster Your Events </a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/reports.php" target="_blank"><i class="fa fa-check"></i> Powerful Workforce Reporting </a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/filemanager.php" target="_blank"><i class="fa fa-check"></i> File Manager</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/pagesphrases.php" target="_blank"><i class="fa fa-check"></i> Custom Phrases</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/checksheets.php" target="_blank"><i class="fa fa-check"></i> Check In Sheets</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/certificates.php" target="_blank"><i class="fa fa-check"></i> Workforce Certificate Creator</a></li>
+           
+            <li class="list-group-item-2"></li>
+            
+          </ul>
+           <div class="border-bottom"></div>
+        </div>
+        </div>  
+      </div>
+      <!-- /item -->
+
+      <!-- item -->
+      <div class="col-md-3 text-center">
+        <div class="box">
+        <div class="panel panel-pricing box">
+          <div class="panel-heading">
+            <i class="fa fa-star icon"></i>
+            <h3>Premium</h3>
+          </div>
+          <div class="panel-body text-center">
+            <p class="lead">Starting from</p>
+            <p><strong>$499 per month</strong></p>
+          </div>
+          <div class="panel-footer">
+            <a class="btn btn-lg btn-block btn-primary pricing_button" data-plan="premium" href="#">Get Started</a>
+          </div>
+          <div class="panel-body-a text-center">
+            <p class="lead text-muted small" style="color: #555; font-size: 12px;"> For organisations with a growing volunteer database (300 - 2,000), wanting to streamline scheduling for mid size events. </p>
+          </div>
+          <ul class="list-group text-center">
+            <li class="list-group-item first-item"><i class="fa fa-check"></i> Base + </li>
+            <li class="list-group-item"><a href="" target="_blank"><i class="fa fa-check"></i> Unlimited Admins </a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/dashboard.php" target="_blank"><i class="fa fa-check"></i> Workforce Overview Page </a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/groupmanager.php" target="_blank"><i class="fa fa-check"></i> Group Manager</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/checkmode.php" target="_blank"><i class="fa fa-check"></i> Check In and Check Out Mode</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/newsletters.php" target="_blank"><i class="fa fa-check"></i> Campaign Emails (Newsletters)*</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/sms.php"><i class="fa fa-check"></i> SMS Communications*</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/leadermanagement.php"><i class="fa fa-check"></i> Team/Shift Leader Management</a></li>
+            
+            <li class="list-group-item-3"></li>
+
+          </ul>
+           <div class="border-bottom"></div>
+        </div>
+         </div> 
+      </div>
+      <!-- /item -->
+
+      <!-- item -->
+      <div class="col-md-3 text-center">
+        <div class="box">
+        <div class="panel panel-pricing box">
+          <div class="panel-heading">
+            <i class="fa fa-globe icon"></i>
+            <h3>Enterprise</h3>
+          </div>
+          <div class="panel-body text-center">
+            <p class="tag-text"><strong>Get in Touch</strong></p>
+          </div>
+          <div class="panel-footer">
+            <a class="btn btn-lg btn-block btn-primary pricing_button" data-plan="enterprise" href="http://eventworkforcegroup.com/technology/demo">Contact Us</a>
+          </div>
+          <div class="panel-body-a text-center">
+            <p class="lead text-muted small"style="padding-bottom: 12px; font-size: 12px;" >Organisations or one-off large scale events wanting full functionality to schedule, train and easily deploy a mass workforce or paid staff teams.</p>
+                        
+          </div>
+          <ul class="list-group text-center">
+            <li class="list-group-item first-item"><i class="fa fa-check"></i> Premium +</li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/support.php" target="_blank"><i class="fa fa-check"></i> 24/7 Support</a></li>
+            <li class="list-group-item"><a href="#" target="_blank"><i class="fa fa-check"></i> Paid Staff Management Functionality</a></li> 
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/reports.php" target="_blank"><i class="fa fa-check"></i> Customised Reporting</a></li>
+            <li class="list-group-item"><a href="#" target="_blank"><i class="fa fa-check"></i> Rosterythm Shift Allocations</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/trainingmanager.php" target="_blank"><i class="fa fa-check"></i>Training Modules</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/eventseries.php" target="_blank"><i class="fa fa-check"></i>Event Series Branding</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/surveys.php" target="_blank"><i class="fa fa-check"></i>Survey Manager</a></li>
+            <li class="list-group-item"><a href="http://www.eventworkforcegroup.com/technology/interview.php" target="_blank"><i class="fa fa-check"></i>Interview Platform</a></li>
+          </ul>
+           <div class="border-bottom"></div>
+        </div>
+       </div>   
+      </div>
+      <!-- /item -->
+
+    </div>
+  </div>
+</section>
+<!-- /Plans -->
+
+<!-- ADD A ENQUIRE NOW BOX -->
+
+<div class="container-fluid grey-box">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 text-center">
+      <h2>Can't see a package to suit?</h2>
+        <p>Submit a request and we will be in touch</p>
+      <p class="text-muted lead">*Additional Fees apply based on usage.</p>
+          <a class="btn btn-lg btn-primary" href="http://eventworkforcegroup.com/technology/demo.html">Contact Us</a>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include $file_base."template_files/footer.php"; ?>
--- template_files/header.php
+++ template_files/header.php
@@ -99,8 +99,14 @@ src="https://www.facebook.com/tr?id=114806989013061&ev=PageView&noscript=1"
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="hvr-underline-from-center"> <a href="<?php echo $file_base; ?>index">Home</a> </li>
-                    
+                     <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Home<span class="caret"></span></a>
+
+                           <ul class="dropdown-menu">
+                             <li><a href="<?php echo $file_base; ?>index">Home</a></li>
+                            <li><a href="http://eventworkforcegroup.com/">EWG Group</a></li>
+            </ul>
+                    <li> <a href="pricing.php">Pricing</a></li>
                     
                        <!--DROP DOWN NAV-->
                         <li class="dropdown">
