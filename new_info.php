e7573f36b0a5ec13ee6b495c3acde7c149169405
chrisgrant <chris.grant@grantet.com.au>
2017-07-26 06:47:03 +1000

    test tech page

---
+++ new_info.php
@@ -0,0 +1,114 @@
+<?php
+$file_base = "";
+$title = "this is a new page";
+$desc = "";
+$_GET['admin'] = true;
+include $file_base."template_files/header.php";
+
+?> 
+   <!--THE FEATURETTES -->
+  <div class="content-section-a white-top">
+    <div class="container">
+      <div class="row featurette">
+        <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
+          <h1>Thanks for being around for a year KARIAH</h1>
+          <p>From everyone, we are super grateful</p>
+          <p>->Super cute photo of us</p>
+        </div>
+        <div class="col-md-5" align="center" data-aos="fade-left" data-aos-once="true">
+          <img class="img-responsive center-block" src="assets/images/kariah-anniversary/k1.jpg" alt="Training Portal on Computer">
+        </div>
+      </div>
+    </div>
+  </div>
+        <!--BIG QUOTE--> 
+    <div id="learnMore"></div>
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
+            <h2 class="featurette-heading">So I went and found some photos</h2>
+            <p class="tech-lead">and borrowed one of petes layouts</p>
+          </div>
+            <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
+            <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k2.jpg" alt="Training Portal on Computer">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="content-section-d">
+        <div class="container">
+           <div class="row">
+                <div class="col-md-2 quote-image" align="center" data-aos="fade-left" data-aos-once="true">
+                    
+                </div>
+                <div class="col-md-10" data-aos="fade-left" data-aos-once="true">
+                     <blockquote class="quote" style='font-size:4em'>
+                       TO SAY THANKYOU
+                                            </blockquote>  
+                </div>
+            </div><!--END ROW-->
+        </div> <!--/.container -->
+    </div><!-- /.content-section-b -->     
+
+      <div class="content-section-a white">
+        <div class="container right">
+          <div class="row featurette">
+            <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
+           <h2 class="featurette-heading">Big Year</h2>
+             <h2>The way you haven taken to your role has been amazing. You have gotten out of your comfort zone & learnt some code, and implements a number of projects, which have made our platform so much better</h2>
+              <p>&nbsp;</p>
+            </div>
+            <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
+              <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k4.jpg" alt="Health and Safety">
+            </div>
+          </div>
+      </div>
+    </div>
+      
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
+            <h2 class="featurette-heading">Side Note</h2>
+             <h2>FYI, this photo isnt about either of us, except I can't beleive BOB was at an event and somehow we werent!!.</h2>
+              <p>&nbsp;</p>
+          </div>
+            <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
+            <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k3.jpg" alt="Shift Specific Training">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="content-section-a white">
+        <div class="container">
+          <div class="row featurette">
+            <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
+              <h2>The team thanks you</h2>
+              <p>I wanted to find an up to date photo but...we dont have one with the WHOLE team, so I went back to when i guess we all became one</p>
+            </div>
+            <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
+              <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k5.png" alt="Training Tracker">
+            </div>
+          </div>
+      </div>
+    </div>
+      
+      <div class="content-section-b blue">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
+             <h2>Last note</h2>
+              <p style='color:white'>Super proud of what you have done for us in the last year and just want you to know we are grateful for much you care and the effort you put in. Thanks from me shan and bennett</p>
+          </div>
+            <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
+            <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k6.jpg" alt="Training On The Go">
+            </div>
+          </div>
+        </div>
+  </div><!-- /END THE FEATURETTES --> 
+       
+
+<?php include $file_base."template_files/footer.php"; ?>
