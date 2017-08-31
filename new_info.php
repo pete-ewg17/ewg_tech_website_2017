7c31ba972ace4b1742849943988f606dcf69820a
Chris Grant <chris@eventworkforcegroup.com>
2017-08-31 17:50:57 -0400

    small updates
    benno content required

--- new_info.php
+++ new_info.php
@@ -11,9 +11,9 @@ include $file_base."template_files/header.php";
     <div class="container">
       <div class="row featurette">
         <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
-          <h1>Thanks for being around for a year KARIAH</h1>
-          <p>From everyone, we are super grateful</p>
-          <p>->Super cute photo of us</p>
+          <h1>Can you believe it?</h1>
+          <p>It really seems like just yesterday</p>
+          <p>Thanks for being part of the EWG team for 2 years ML</p>
         </div>
         <div class="col-md-5" align="center" data-aos="fade-left" data-aos-once="true">
           <img class="img-responsive center-block" src="assets/images/kariah-anniversary/k1.jpg" alt="Training Portal on Computer">
@@ -27,8 +27,8 @@ include $file_base."template_files/header.php";
        <div class="container">
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
-            <h2 class="featurette-heading">So I went and found some photos</h2>
-            <p class="tech-lead">and borrowed one of petes layouts</p>
+            <h2 class="featurette-heading">A lots changed in the last few years, especially around here. So thanks for being a part of the journey so far. We couldnt have done it without you. </h2>
+            <p class="tech-lead"></p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
             <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k2.jpg" alt="Training Portal on Computer">
@@ -45,7 +45,7 @@ include $file_base."template_files/header.php";
                 </div>
                 <div class="col-md-10" data-aos="fade-left" data-aos-once="true">
                      <blockquote class="quote" style='font-size:4em'>
-                       TO SAY THANKYOU
+                       SO THANKYOU FROM everyone in the EWG family
                                             </blockquote>  
                 </div>
             </div><!--END ROW-->
@@ -56,9 +56,8 @@ include $file_base."template_files/header.php";
         <div class="container right">
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
-           <h2 class="featurette-heading">Big Year</h2>
-             <h2>The way you haven taken to your role has been amazing. You have gotten out of your comfort zone & learnt some code, and implements a number of projects, which have made our platform so much better</h2>
-              <p>&nbsp;</p>
+           <h2 class="featurette-heading">From Shannan</h2>
+              <p>J Lang Dang. So happy you chose to join EWG over Hilts two years ago today!!! Bloody hilts...haha. Massive thank you for everything you have done for the business and all of your Workforcers over your journey. Your passion for the role and commitment to providing the very best staff the industry has to offer is why you have excelled so much. Unfortunately I don't think you will ever understand the massive impact you have had on thousands of people's lives over the last two years but I hope you have found enjoyment from all of the great stories you have created so far. The future is exciting here and I really look forward to seeing you grow into the role even more, as you have done so much since you started. To many good years ahead</p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
               <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k4.jpg" alt="Health and Safety">
@@ -71,9 +70,8 @@ include $file_base."template_files/header.php";
        <div class="container">
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
-            <h2 class="featurette-heading">Side Note</h2>
-             <h2>FYI, this photo isnt about either of us, except I can't beleive BOB was at an event and somehow we werent!!.</h2>
-              <p>&nbsp;</p>
+            <h2 class="featurette-heading">And Bennett</h2>
+              <p>Bennett write your note here I hope michaela doesnt see this before you get your note in. michaela if you're reading this, Bennett is wri</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
             <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k3.jpg" alt="Shift Specific Training">
@@ -86,11 +84,11 @@ include $file_base."template_files/header.php";
         <div class="container">
           <div class="row featurette">
             <div class="col-md-7" data-aos="fade-left" data-aos-once="true">
-              <h2>The team thanks you</h2>
-              <p>I wanted to find an up to date photo but...we dont have one with the WHOLE team, so I went back to when i guess we all became one</p>
+              <h2>And Chris</h2>
+              <p>Yo Michaela. What a change from the old days in richmond. I will admit I miss walking across the park to get a coffee, but I think the south melbourne change has been one for the better with the whole natural light in the office for one! Thank you so much for everything you bring to our team. The academy has been owned by you like never before and the way you care about all the workforcers never seices (<-thats not the right spelling) to amaze me. I know we are different people what with me being a jerk sometimes, but i love that we can still work so well together. Looking forward to seeing you in a week and for many more years. </p>
             </div>
             <div class="col-md-5" data-aos="fade-left" data-aos-once="true">
-              <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k5.png" alt="Training Tracker">
+              <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k6.jpg" alt="Training Tracker">
             </div>
           </div>
       </div>
@@ -100,11 +98,11 @@ include $file_base."template_files/header.php";
        <div class="container">
         <div class="row">
           <div class="col-md-7 col-md-push-5" data-aos="fade-right" data-aos-once="true">
-             <h2>Last note</h2>
-              <p style='color:white'>Super proud of what you have done for us in the last year and just want you to know we are grateful for much you care and the effort you put in. Thanks from me shan and bennett</p>
+             <h2>Authors note</h2>
+              <p style='color:white'>Look forward to seeing you in a week Micky</p>
           </div>
             <div class="col-md-5 col-md-pull-7" data-aos="fade-right" data-aos-once="true">
-            <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k6.jpg" alt="Training On The Go">
+            <img class="featurette-image img-responsive img-rounded" src="assets/images/kariah-anniversary/k5.jpg" alt="Training On The Go">
             </div>
           </div>
         </div>
